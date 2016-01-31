e74ce93bdf958b8728480ecf6bd0f815ff26ef0f
Nick Cox <nick@nickcox.me>
2016-01-31 10:05:52 -0800

    send SMS

--- weather_alert.go
+++ weather_alert.go
@@ -3,6 +3,7 @@ package main
 import _ "github.com/joho/godotenv/autoload"
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/jmoiron/jsonq"
@@ -13,11 +14,32 @@ import (
 )
 
 var url = "http://api.openweathermap.org/data/2.5/forecast?id=" + os.Getenv("CITY_ID") + "&appid=" + os.Getenv("API_KEY") + "&units=imperial"
+var ifTTTAPIURL = "https://maker.ifttt.com/trigger/" + eventType + "/with/key/" + os.Getenv("IFTTT_API_KEY")
+
+const eventType = "weather_alert"
 
 func perror(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func jsonBody(temp float64) *bytes.Buffer {
+	jsonReq := fmt.Sprintf(`{"value1":"%s"}`, fmt.Sprintf("%.2f", temp))
+	fmt.Println(jsonReq)
+	jsonStr := []byte(jsonReq)
+	reqBody := bytes.NewBuffer(jsonStr)
+	return reqBody
+}
+
+func sendSMS(temp float64) {
+	req, _ := http.NewRequest("POST", ifTTTAPIURL, jsonBody(temp))
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	perror(err)
+	fmt.Println(resp.Status)
 }
 
 func getWeather(url string) []byte {
@@ -36,5 +58,5 @@ func main() {
 	dec.Decode(&data)
 	jq := jsonq.NewQuery(data)
 	temp, _ := jq.Float("list", "0", "main", "temp_min")
-	fmt.Println(fmt.Sprintf("The temperature will be %.2f", temp))
+	sendSMS(temp)
 }
